a4ebbdb8280ce405b5e38e1979d3d80311570cd2
MarcoSolarino <marcosolarino@live.it>
2020-03-21 17:06:58 +0100

    3rd commit:
    - class Node
    - decision_tree_learning function
    - importance now return the attribute with the max gain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import copy
 import pandas as pd
 import math
+import numpy
 
 
 def create_data_set(file):
@@ -20,8 +21,9 @@ def create_data_set(file):
 def attribute_values(attribute_index, examples):
     values = []
     for i in range(len(examples.index)):
-        if values.__contains__(examples.iloc[i, attribute_index]) is False:
-            values.append(examples.iloc[i, attribute_index])
+        index = examples.columns.get_loc(attribute_index)
+        if values.__contains__(examples.iloc[i, index]) is False:
+            values.append(examples.iloc[i, index])
     return values
 
 
@@ -51,15 +53,43 @@ def remainder(attribute_index, examples):
     values = attribute_values(attribute_index, examples)
     r = 0
     for v in values:
-        c = examples.columns[attribute_index]
-        ex_attr_v = examples[(examples[c] == v)]
-        r += prob_attribute_value(attribute_index, v, examples) * entropy(len(ex_attr_v.columns) - 2, ex_attr_v)
+        ex_attr_v = examples[(examples[attribute_index] == v)]
+        r += prob_attribute_value(attribute_index, v, examples) * entropy("class", ex_attr_v)
     return r
 
 
-def importance(attribute_index, examples):
-    gain = entropy(len(examples.columns) - 2, examples) - remainder(attribute_index, examples)
-    return gain
+def importance(examples):
+    gain_vector = []
+    for i in examples.columns:
+        gain = entropy("class", examples) - remainder(i, examples)
+        gain_vector.append(copy.copy(gain))
+    find_max = numpy.array(copy.copy(gain_vector))
+    result = numpy.where(numpy.amax(find_max))
+    return result
+
+
+class Node:
+    def __init__(self, examples):
+        self.examples = examples
+        self.subtree = []
+
+
+def decision_tree_learning(examples, attributes, pater_examples):
+    if examples.empty():
+        return "vuoto!"  # TODO funzione plurality_value()
+    result = attribute_values("class", examples)
+    if len(result) == 1:
+        return result[0]
+    if len(attributes) == 0:
+        return "rivuoto!"  # TODO anche qui plurality_value()
+    attribute = examples.columns[importance(examples)]
+    T = Node(examples)
+    values = attribute_values(attribute, examples)
+    for v in values:
+        exs = examples[examples[attribute] == v]
+        T.subtree.append(decision_tree_learning(exs, attributes.remove(attribute), examples))
+    return T
+
 
 
 # math.log(x,2)
@@ -67,5 +97,5 @@ def importance(attribute_index, examples):
 
 
 data = create_data_set("car.data")
-print(importance(0, data))
+print(importance(data))
 
