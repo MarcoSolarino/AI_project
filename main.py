8843aa4a100346b6832496ec21d7d0d071f14b29
MarcoSolarino <marcosolarino@live.it>
2020-03-23 11:07:18 +0100

    4th commit:
    fixed methods:
    - attribute_values
    - remainder
    - importance
    - decision_tree_learning
    
    added methods:
    - split_data_set
    - plurality_values
    - get_attribute_list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import copy
 import pandas as pd
 import math
 import numpy
+import random
 
 
 def create_data_set(file):
@@ -13,7 +14,7 @@ def create_data_set(file):
     weight = []
     for i in range(len(data.index)):
         weight.append(1)
-    data['Weight'] = weight
+    data['weight'] = weight
     return data
 
 
@@ -21,10 +22,15 @@ def create_data_set(file):
 def attribute_values(attribute_index, examples):
     values = []
     for i in range(len(examples.index)):
-        index = examples.columns.get_loc(attribute_index)
-        if values.__contains__(examples.iloc[i, index]) is False:
-            values.append(examples.iloc[i, index])
+        #  index = examples.columns.get_loc(attribute_index)
+        if values.__contains__(examples.iloc[i][attribute_index]) is False:
+            values.append(examples.iloc[i][attribute_index])
     return values
+
+
+def split_data_set(attribute_index, examples):
+    df_tuple = dict(tuple(examples.index.groupby(attribute_index)))
+    return df_tuple
 
 
 # calcola la probabilità che un esempio abbia per valore dell'attributo indicato da attribute_index value
@@ -53,19 +59,23 @@ def remainder(attribute_index, examples):
     values = attribute_values(attribute_index, examples)
     r = 0
     for v in values:
-        ex_attr_v = examples[(examples[attribute_index] == v)]
-        r += prob_attribute_value(attribute_index, v, examples) * entropy("class", ex_attr_v)
+        name = examples.columns.values[attribute_index]
+        ex_attr_v = examples[(examples[name] == v)]
+        r += prob_attribute_value(attribute_index, v, examples) * entropy(len(examples.columns) - 2, ex_attr_v)
     return r
 
 
-def importance(examples):
+def importance(attributes, examples):
     gain_vector = []
-    for i in examples.columns:
-        gain = entropy("class", examples) - remainder(i, examples)
+    for j in attributes:
+        col_index = examples.columns.get_loc(j)
+        gain = entropy(len(examples.columns.values) - 2, examples) - remainder(col_index, examples)
         gain_vector.append(copy.copy(gain))
     find_max = numpy.array(copy.copy(gain_vector))
-    result = numpy.where(numpy.amax(find_max))
-    return result
+    result = numpy.amax(find_max)
+    for i in range(len(gain_vector)):
+        if result == gain_vector[i]:
+            return attributes[i]
 
 
 class Node:
@@ -74,28 +84,55 @@ class Node:
         self.subtree = []
 
 
-def decision_tree_learning(examples, attributes, pater_examples):
-    if examples.empty():
-        return "vuoto!"  # TODO funzione plurality_value()
-    result = attribute_values("class", examples)
+def plurality_value(examples):
+    max = examples["class"].mode()
+    if len(max) == 1:
+        return max[0]
+    else:
+        return random.choice(max)
+
+
+def get_attributes_list(examples):
+    list = []
+    for i in examples.columns.values:
+        if (i != "class") and (i != "weight"):
+            list.append(i)
+    return list
+
+
+def decision_tree_learning(examples, attrib, pater_examples):
+    print(attrib)
+    if examples.empty:
+        return plurality_value(pater_examples)
+    result = attribute_values(len(examples.columns.values) - 2, examples)
     if len(result) == 1:
         return result[0]
-    if len(attributes) == 0:
-        return "rivuoto!"  # TODO anche qui plurality_value()
-    attribute = examples.columns[importance(examples)]
-    T = Node(examples)
-    values = attribute_values(attribute, examples)
+    if attrib is None:
+        return plurality_value(examples)
+    attribute = importance(attrib, examples)
+    tree = Node(examples)
+    col_index = examples.columns.get_loc(attribute)
+    values = attribute_values(col_index, examples)
     for v in values:
         exs = examples[examples[attribute] == v]
-        T.subtree.append(decision_tree_learning(exs, attributes.remove(attribute), examples))
-    return T
+        new_attributes = copy.copy(attrib)
+        new_attributes.remove(attribute)
+        tree.subtree.append(decision_tree_learning(exs, new_attributes, examples))
+    return tree
 
 
-
-# math.log(x,2)
 # here starts the main
 
 
 data = create_data_set("car.data")
-print(importance(data))
+
+attributes = get_attributes_list(data)
+
+vettore_prova = ['attr0', 'attr1', 'attr2', 'attr4']
+
+# print(importance(vettore_prova, data))
+
+decision_tree_learning(data, attributes, None)
+
+
 
